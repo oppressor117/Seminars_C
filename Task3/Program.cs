308a9b06e740b4d3cead6d9ff0fc8b1edfea4709
oppressor117 <oppressor117@gmail.com>
2022-12-06 14:30:15 +0300

    Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,19 @@
+﻿string? inputLine = Console.ReadLine();
+
+if(inputLine!=null)
+{
+    string[]dayWeek = new string [7];
+    dayWeek[0] = "Понедельник";
+    dayWeek[1] = "Вторник";
+    dayWeek[2] = "Среда";
+    dayWeek[3] = "Четверг";
+    dayWeek[4] = "Пятница";
+    dayWeek[5] = "Суббота";
+    dayWeek[6] = "Воскресенье";
+
+    int inputNumber = int.Parse(inputLine);
+
+    string outDayWeek = dayWeek[inputNumber-1];
+
+    Console.WriteLine(outDayWeek);
+}
