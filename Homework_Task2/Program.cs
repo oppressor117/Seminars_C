eb4c075fb5e8b58ce3e4fa4861650f39afb0ae13
oppressor117 <oppressor117@gmail.com>
2022-12-06 15:52:52 +0300

    Solution for Task2 added

---
+++ Homework_Task2/Program.cs
@@ -0,0 +1,21 @@
+﻿//Считываем данные с консоли
+string? num1Line = Console.ReadLine();
+string? num2Line = Console.ReadLine();
+
+//Проверяем, чтобы данные не были пустыми
+if(num1Line!=null&&num2Line!=null)
+{
+    //Парсим введенные числа
+    int num1 = int.Parse(num1Line);
+    int num2 = int.Parse(num2Line);
+
+    //Сравниваем
+    if(num1 > num2)
+    {
+        Console.WriteLine(num1);
+    }
+    else
+    {
+        Console.WriteLine(num2);
+    }    
+}
