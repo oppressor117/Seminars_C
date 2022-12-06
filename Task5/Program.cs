5e9f099b475c313d7be516a8020479a82a813963
oppressor117 <oppressor117@gmail.com>
2022-12-06 15:19:29 +0300

    Task5

---
+++ Task5/Program.cs
@@ -0,0 +1,20 @@
+﻿string? inputLine = Console.ReadLine();
+
+if(inputLine!=null)
+{
+        int inputNumber = int.Parse(inputLine);
+
+        int startNumber = inputNumber*(-1);
+
+        string outLine = string.Empty;
+
+        while(startNumber<inputNumber)
+        {
+            outLine = outLine + startNumber + ", ";
+            startNumber++;
+            // startNumber=startNumber+1
+        }
+    outLine = outLine + inputNumber;
+    
+    Console.WriteLine(outLine);
+}
