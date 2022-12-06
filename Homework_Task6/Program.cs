6887a743dd365af3773a463e92d1692e8c0282ab
oppressor117 <oppressor117@gmail.com>
2022-12-06 16:56:54 +0300

    Solution for Task6 added

---
+++ Homework_Task6/Program.cs
@@ -0,0 +1,18 @@
+﻿//Считываем данные с консоли
+string? inputNumberLine = Console.ReadLine();
+
+//Проверяем, чтобы данные не были пустыми
+if(inputNumberLine!=null)
+{
+    //Парсим введенное число
+    int inputNumber = int.Parse(inputNumberLine);
+    //Условие
+    if(inputNumber % 2 == 0)
+    {
+        Console.WriteLine("Четное");
+    }
+    else
+    {
+        Console.WriteLine("Нечетное");
+    }
+}
