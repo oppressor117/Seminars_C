c3a4b6fcc75d8f6e9398f49a26f29184e1b2f7f2
oppressor117 <oppressor117@gmail.com>
2022-12-06 18:16:56 +0300

    Solution for Task8 added

---
+++ Homework_Task8/Program.cs
@@ -0,0 +1,21 @@
+﻿//Считываем данные с консоли
+string? inputNumberLine = Console.ReadLine();
+
+//Проверяем, чтобы данные не были пустыми
+if(inputNumberLine!=null)
+
+{
+    //Парсим введенное число
+    int inputNumber = int.Parse(inputNumberLine);
+    int evenNumber = 2;
+    string res = string.Empty;
+
+    while (evenNumber <= inputNumber)
+    {
+        res = res + evenNumber + ", ";
+        evenNumber = evenNumber + 2;
+    }
+
+Console.WriteLine(res);
+
+}
