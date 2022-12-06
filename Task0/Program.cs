30b57b8791befba96e9bb8a70b3aa34fddb4bce9
oppressor117 <oppressor117@gmail.com>
2022-12-06 12:55:58 +0300

    Task0

---
+++ Task0/Program.cs
@@ -0,0 +1,18 @@
+﻿
+//Считываем данные с консоли
+string? inputLine = Console.ReadLine();
+
+//Проверяем, чтобы данные не были пустыми
+if(inputLine!=null)
+{
+    //Парсим введенное число
+    int inputNumber = int.Parse(inputLine);
+
+    //Находим квадрат числа
+    int outNumber = inputNumber*inputNumber;
+    //int outNumber = (int)Math.Pow(inputNumber,2));
+
+    //Выводим данные в консоль
+    Console.WriteLine(outNumber);
+    //Console.WriteLine(Math.Pow(int.Parse(inputLine),2));
+}
