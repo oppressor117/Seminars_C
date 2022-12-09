90abb76c0f11ec67a9d5ab377c4d7c23dc9850fd
oppressor117 <oppressor117@gmail.com>
2022-12-09 19:33:03 +0300

    Solution for Homework_Task10 added

---
+++ Homework_Task10/Program.cs
@@ -0,0 +1,13 @@
+﻿int inputNumber = int.Parse(Console.ReadLine()??"0");
+
+if((inputNumber<1000)&&(inputNumber>99))
+{
+    int resultNumber = (inputNumber/10)%10;
+
+    Console.WriteLine(resultNumber);
+
+}
+else
+{
+    Console.WriteLine("Вы ввели не трехзначное число!");
+}
