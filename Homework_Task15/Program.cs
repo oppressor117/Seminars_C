c4aba9ad6aff0c7371269f09946004eb1f18a962
oppressor117 <oppressor117@gmail.com>
2022-12-09 19:55:04 +0300

    Solution for Homework_Task15 added

---
+++ Homework_Task15/Program.cs
@@ -0,0 +1,13 @@
+﻿int inputNumber = int.Parse(Console.ReadLine()??"0");
+
+if(inputNumber>0&&inputNumber<8)
+{
+    if(inputNumber>5);
+
+    Console.WriteLine("Это рабочий день!");
+
+}
+else
+{
+    Console.WriteLine("Это выходной!");
+}
