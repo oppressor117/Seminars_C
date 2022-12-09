2bf89761ecbcaef9a34694993fe1296faf96b72d
oppressor117 <oppressor117@gmail.com>
2022-12-09 18:22:39 +0300

    Solution for Task11 added

---
+++ Task11/Program.cs
@@ -0,0 +1,14 @@
+﻿//Создаем экземляр класса рандомайзера
+System.Random numberSintezator = new System.Random();
+
+int number = new Random().Next(100, 1000);
+
+Console.WriteLine(number);
+
+//int firstNumber = number/100;
+//int thirdNumber = number%10;
+
+//int resultNumber = firstNumber*10+thirdNumber; 
+int resultNumber = (number/100)*10 + number%10;
+
+Console.WriteLine(resultNumber);
