67422c9804470e4d999457fd2f7398d69c7c9ff1
oppressor117 <oppressor117@gmail.com>
2022-12-09 18:46:33 +0300

    Solution for Task14 added

---
+++ Task14/Program.cs
@@ -0,0 +1,13 @@
+﻿int inputNumber = int.Parse(Console.ReadLine()??"0");
+
+int firstResultNumber = inputNumber%7;
+int secondResultNumber = inputNumber%23;
+
+if((firstResultNumber == 0)&&(secondResultNumber==0))
+{
+    Console.WriteLine("кратно");
+}
+else
+{
+    Console.WriteLine("некратно");
+}
