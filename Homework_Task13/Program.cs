1b83707429001cedf32e9c0d5da5050070361efe
oppressor117 <oppressor117@gmail.com>
2022-12-09 19:40:07 +0300

    Solution for Homework_Task13 added

---
+++ Homework_Task13/Program.cs
@@ -0,0 +1,13 @@
+﻿int inputNumber = int.Parse(Console.ReadLine()??"0");
+
+if(inputNumber>99)
+{
+    char[] digits = inputNumber.ToString().ToCharArray();
+
+    Console.WriteLine(digits[2]);
+
+}
+else
+{
+    Console.WriteLine("Третьей цифры нет!");
+}
