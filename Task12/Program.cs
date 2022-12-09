2d3ec7131f27febd04ec3ac6f28cee3802ad2540
oppressor117 <oppressor117@gmail.com>
2022-12-09 18:38:24 +0300

    Solution for Task12 added

---
+++ Task12/Program.cs
@@ -0,0 +1,14 @@
+﻿int firstNumber = int.Parse(Console.ReadLine()??"0");
+
+int secondNumber = int.Parse(Console.ReadLine()??"0");
+
+int resultNumber = secondNumber % firstNumber;
+
+if(resultNumber == 0)
+{
+    Console.WriteLine("кратно");
+}
+else
+{
+    Console.WriteLine("некратно, " + resultNumber);
+}
