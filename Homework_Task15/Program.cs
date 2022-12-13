01752a4c82c3b6bf7ae9950a304c599c880e8fca
oppressor117 <oppressor117@gmail.com>
2022-12-13 18:29:41 +0300

    New solution for Homework_Task15 added

--- Homework_Task15/Program.cs
+++ Homework_Task15/Program.cs
@@ -1,13 +1,34 @@
 ﻿
 int inputNumber = int.Parse(Console.ReadLine()??"0");
 if(inputNumber>0&&inputNumber<8)
+//{
+//if(inputNumber<6)
+//{
+//Console.WriteLine("Это рабочий день!");
+//} 
+//else
+//{
+//Console.WriteLine("Это выходной!");
+//}
+//}
 {
-if(inputNumber<6)
+Dictionary<int, string> dict = new Dictionary<int, string>
 {
-Console.WriteLine("Это рабочий день!");
-} 
-else
-{
-Console.WriteLine("Это выходной!");
+    {1, "рабочий день"},
+    {2, "рабочий день"},
+    {3, "рабочий день"},
+    {4, "рабочий день"},
+    {5, "рабочий день"},
+    {6, "выходной день"},
+    {7, "выходной день"},
+
+};
+
+Console.WriteLine(dict[inputNumber]);
+
 }
+
+else 
+{
+    Console.WriteLine("Это не день недели!");
 }
