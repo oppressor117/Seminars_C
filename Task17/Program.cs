58048f90242109ac900a1f190e5d63291d14bb4f
oppressor117 <oppressor117@gmail.com>
2022-12-13 19:03:14 +0300

    Solution for Task17 added

---
+++ Task17/Program.cs
@@ -0,0 +1,28 @@
+﻿//Вводим данные
+//Console.WriteLine("Введите коорлинату X");
+//int coordX = int.Parse(Console.ReadLine()??"0");
+
+//Console.WriteLine("Введите коорлинату X");
+//int coordY = int.Parse(Console.ReadLine()??"0");
+
+//Метод читает данные от пользователя
+
+
+int coordX = ReadData ("Введите координату X");
+int coordY = ReadData ("Введите координату Y");
+PrintQuterTest();
+
+int ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    return int.Parse(Console.ReadLine()??"0");
+}
+void PrintQuterTest() //Метод определяет четверть по координатам
+{
+if(coordX > 0 && coordY > 0) Console.WriteLine("Точка в четверти 1");
+if(coordX > 0 && coordY < 0) Console.WriteLine("Точка в четверти 2");
+if(coordX < 0 && coordY < 0) Console.WriteLine("Точка в четверти 3");
+if(coordX < 0 && coordY > 0) Console.WriteLine("Точка в четверти 4");
+}
+
+
