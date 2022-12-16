1f2e33efc13bbd3c8b92b14865652cc8c0d4a4ed
oppressor117 <oppressor117@gmail.com>
2022-12-16 19:49:59 +0300

    Solution for Homework_Task27 added

---
+++ Homework_Task27/Program.cs
@@ -0,0 +1,28 @@
+﻿int num = ReadData ("Введите число: ");
+
+int res = SumDigit(num);
+
+PrintResult("Сумма цифр в числe: " + res);
+
+int ReadData(string line)
+{
+    Console.WriteLine(line);
+    return int.Parse(Console.ReadLine()??"0");
+}
+
+void PrintResult(string line)
+{
+    Console.WriteLine(line);
+}
+
+int SumDigit(int num)
+{
+    int res = 0;
+    while(num > 0)
+    {
+    res += num%10;
+    num = num/10;
+    }
+    return res;
+}
+
