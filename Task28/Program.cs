6af5a0d63a1772c78341f999238d9a0e56933f95
oppressor117 <oppressor117@gmail.com>
2022-12-16 18:40:09 +0300

    Solution for Task28 added

---
+++ Task28/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Numerics;
+
+int num = ReadData ("Введите число: ");
+
+BigInteger res = CalcFact(num);
+PrintResult("Факториал: " + res);
+
+int ReadData(string res)
+{
+    Console.WriteLine(res);
+    return int.Parse(Console.ReadLine()??"0");
+}
+BigInteger CalcFact(int num)
+{
+    BigInteger res = 1;
+    for(int i = 1; i <= num; i++)
+    {
+        res *= i;
+    }
+    return res;
+}
+
+void PrintResult(string res)
+{
+    Console.WriteLine(res);
+}
