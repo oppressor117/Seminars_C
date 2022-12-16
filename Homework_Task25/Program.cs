e3639896329d55d2d96735522b9dfcf667d91f52
oppressor117 <oppressor117@gmail.com>
2022-12-16 19:27:37 +0300

    Solution for Homework_Task25 added

---
+++ Homework_Task25/Program.cs
@@ -0,0 +1,26 @@
+﻿int numA = ReadData ("Введите число A: ");
+int numB = ReadData ("Введите число B: ");
+
+int res = Pow(numA, numB);
+PrintData(res);
+
+int ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    return int.Parse(Console.ReadLine()??"0");
+}
+
+int Pow(int A, int B)
+{
+    int res = 1;
+    for(int i = 1; i <= B; i++)
+    {
+        res = A * res;
+    }
+    return res;
+}
+
+void PrintData(int res)
+{
+    Console.WriteLine(res);
+}
