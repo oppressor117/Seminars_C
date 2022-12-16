98e14ade3863a484f6060071c90817f200394eb0
oppressor117 <oppressor117@gmail.com>
2022-12-16 16:00:37 +0300

    Solution for Task22 added

---
+++ Task22/Program.cs
@@ -0,0 +1,30 @@
+﻿int N = ReadData ("Введите число N");
+
+string outline = LineBuilder(N, 1);
+PrintData("", outline);
+outline = LineBuilder(N, 2);
+PrintData("", outline);
+
+int ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    return int.Parse(Console.ReadLine()??"0");
+}
+
+string LineBuilder(int N, int Pow)
+
+{
+    string outline = String.Empty;
+    for (int i=1; i < N; i++)
+    {
+        outline = outline + Math.Pow(i, Pow) + " ";
+    }
+    outline = outline + Math.Pow(N, Pow);
+    return outline;
+}
+
+void PrintData(string msg, string res)
+{
+    Console.WriteLine(msg + res);
+}
+
