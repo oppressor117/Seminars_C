8a8b582f9c22fbe9dce4775b9cc644b9af4c30ef
oppressor117 <oppressor117@gmail.com>
2022-12-16 14:46:33 +0300

    Solution for Task18 added

---
+++ Task18/Program.cs
@@ -0,0 +1,22 @@
+﻿
+int quarter = ReadData ("Введите номер четверти");
+
+PrintAnswer(quarter);
+
+int ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    return int.Parse(Console.ReadLine()??"0");
+}
+void PrintAnswer(int num) //Метод определяет четверть по координатам
+{
+    if(num > 0 && num < 5)
+    {
+        if(num == 1) Console.WriteLine("x>0, y>0");
+        if(num == 2) Console.WriteLine("x>0, y<0");
+        if(num == 3) Console.WriteLine("x<0, y<0");
+        if(num == 4) Console.WriteLine("x<0, y>0");
+    }
+    else Console.WriteLine("Вы ввели не номер четверти!");
+
+}
