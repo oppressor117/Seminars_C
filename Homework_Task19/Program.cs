d0e117ccd438f09321e38d2ff5cc5912c7dc0b17
oppressor117 <oppressor117@gmail.com>
2022-12-16 17:21:52 +0300

    Solution for Homework_Task19 added

---
+++ Homework_Task19/Program.cs
@@ -0,0 +1,21 @@
+﻿int num = ReadData ("Введите пятизначное число");
+
+bool result = PalinTest(num);
+PrintData(result);
+
+int ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    return int.Parse(Console.ReadLine()??"0");
+
+bool PalinTest(int num)
+{
+    bool result = false;
+    if ((num/10000 == num%10)&&((num/1000)%10 == (num/10)%10)) result = true;
+    return result;
+}
+
+void PrintData(bool result)
+{
+    Console.WriteLine(result);
+}
