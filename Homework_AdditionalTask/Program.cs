ab07c4ab03f03ed5bfe66cf7e94926501b72e12d
oppressor117 <oppressor117@gmail.com>
2022-12-20 16:20:14 +0300

    Solution for Homework_AdditionalTask added

---
+++ Homework_AdditionalTask/Program.cs
@@ -0,0 +1,21 @@
+﻿string str = ReadData ("Введите имена через запятую: ");
+
+PrintResult(" " + NameChoice(str));
+
+string ReadData(string line)
+{
+    Console.WriteLine(line);
+    return Console.ReadLine()??"0";
+}
+
+void PrintResult(string line)
+{
+    Console.WriteLine(line);
+}
+
+string NameChoice(string str)
+{
+    string[] names = str.Split(",");
+    string name = names[new Random().Next(0, names.Length)].Trim();
+    return name;
+}
