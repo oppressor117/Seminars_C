af05a92f53483c56413ae52c6e9d675f67dd5d4a
oppressor117 <oppressor117@gmail.com>
2022-12-20 18:20:26 +0300

    Solution for Task37 added

---
+++ Task37/Program.cs
@@ -0,0 +1,37 @@
+﻿int[] arr = GenArray(123, -100, 100);
+PrintArr(arr);
+Console.WriteLine();
+int[] result = ConvertArray(arr);
+PrintArr(result);
+Console.WriteLine();
+
+int[] GenArray(int len, int minValue, int maxValue)
+{
+    Random rnd = new Random();
+    int[] arr = new int[len];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return arr;
+}
+
+void PrintArr(int[] arr)
+{
+    Console.Write("[");
+    for(int i = 0; i < arr.Length-1; i++)
+    {
+        Console.Write(arr[i]+", ");
+    }
+    Console.WriteLine(arr[arr.Length-1]+"]");
+}
+
+int[] ConvertArray(int[] arr)
+{
+    int[] outArr = new int[arr.Length/2];
+    for(int i = 0; i < arr.Length/2; i++)
+    {
+        outArr[i] = arr[i]*arr[arr.Length - 1 - i];
+    }
+    return outArr;
+}
