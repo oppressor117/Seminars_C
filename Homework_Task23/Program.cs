e91dca90fbfe7b4969cb63d3527a4b23bd73f3a2
oppressor117 <oppressor117@gmail.com>
2022-12-20 13:15:48 +0300

    Solution for Homework_Task23 added

---
+++ Homework_Task23/Program.cs
@@ -0,0 +1,29 @@
+﻿int N = ReadData ("Введите число N");
+
+string outline = LineBuilder(N, 3);
+PrintData("", outline);
+
+
+int ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    return int.Parse(Console.ReadLine()??"0");
+}
+
+string LineBuilder(int N, int Pow)
+
+{
+    string outline = String.Empty;
+    for (int i=1; i < N; i++)
+    {
+        outline = outline + Math.Pow(i, Pow) + ", ";
+    }
+    outline = outline + Math.Pow(N, Pow);
+    return outline;
+}
+
+void PrintData(string msg, string res)
+{
+    Console.WriteLine(msg + res);
+}
+
