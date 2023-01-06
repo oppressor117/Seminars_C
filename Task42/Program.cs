94b5368faaa62371d60f53bf7ad4b1381b4509fd
oppressor117 <oppressor117@gmail.com>
2023-01-06 16:08:51 +0300

    Solution for Task42 added

---
+++ Task42/Program.cs
@@ -0,0 +1,32 @@
+﻿int inputNumber = ReadData ("Введите число: ");
+PrintData(DecToBin(inputNumber));
+PrintData(DecToBinNative(inputNumber));
+PrintData(Convert.ToString(inputNumber, 8));
+PrintData(Convert.ToString(inputNumber, 16));
+
+int ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    return int.Parse(Console.ReadLine()??"0");
+}
+
+void PrintData(string res)
+{
+    Console.WriteLine(res);
+}
+
+string DecToBin(int num) // Преобразование десятичного в двоичную
+{
+    string outBin = string.Empty;
+    while(num>0)
+    {
+        outBin = (num%2) + outBin;
+        num = num/2;
+    }
+    return outBin;
+}
+
+string DecToBinNative(int num)
+{
+    return Convert.ToString(num, 2);
+}
