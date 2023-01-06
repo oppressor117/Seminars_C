607687a08f9305bdcea55836ccb55ef6e38929d5
oppressor117 <oppressor117@gmail.com>
2023-01-06 16:57:52 +0300

    Solution for Homework_Task41 added

---
+++ Homework_Task41/Program.cs
@@ -0,0 +1,25 @@
+﻿int inputNumber = ReadData ("Введите N чисел: ");
+PrintData("Кол-во положительных чисел: ", CountPosNum(inputNumber));
+
+int ReadData(string msg) //Ввод чисел пользователем
+{
+    Console.WriteLine(msg);
+    return int.Parse(Console.ReadLine()??"0");
+}
+
+void PrintData(string msg, int res) //Вывод результата
+{
+    Console.WriteLine(msg+res);
+}
+
+int CountPosNum(int N) //Подсчет кол-ва положительных чисел
+{
+    int res = 0;
+    while(N > 0)
+    {
+        if(ReadData("Введите число: ")>0) res++;
+        N = N - 1;
+    }
+    return res;
+
+}
