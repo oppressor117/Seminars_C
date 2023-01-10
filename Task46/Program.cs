5504da120ae6760a8205ec40908e142fc22c337f
oppressor117 <oppressor117@gmail.com>
2023-01-10 16:11:56 +0300

    Solution for Task46 added

---
+++ Task46/Program.cs
@@ -0,0 +1,58 @@
+﻿
+int ReadData(string line) 
+{
+    Console.WriteLine(line);
+    return int.Parse(Console.ReadLine()??"0");
+}
+
+int[,] Fill2DArray(int countRow, int countColumn, int topBorder, int downBorder) //Универсальный метод генерации и заполнения двумерного массива
+
+{   System.Random rand = new System.Random();
+    int[,] array2D = new int[countRow, countColumn];
+    for(int i = 0; i < countRow; i++)
+    {
+        for(int j = 0; j < countColumn; j++)
+        {
+            array2D[i,j] = rand.Next(topBorder, downBorder + 1);
+        }
+    }
+    return array2D;
+}
+
+void Print2DArray(int[,] matrix) // Печать двумерного массива
+{
+for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i,j]+ " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+ConsoleColor[] col = new ConsoleColor[]{ConsoleColor.Black,ConsoleColor.Blue,ConsoleColor.Cyan,
+                                        ConsoleColor.DarkBlue,ConsoleColor.DarkCyan,ConsoleColor.DarkGray,
+                                        ConsoleColor.DarkGreen,ConsoleColor.DarkMagenta,ConsoleColor.DarkRed,
+                                        ConsoleColor.DarkYellow,ConsoleColor.Gray,ConsoleColor.Green,
+                                        ConsoleColor.Magenta,ConsoleColor.Red,ConsoleColor.White,
+                                        ConsoleColor.Yellow};
+
+void Print2DArrayColor(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.ForegroundColor = col[new System.Random().Next(0,16)];
+            Console.Write(matrix[i, j] + " ");
+            Console.ResetColor(); 
+        }
+        Console.WriteLine();
+    }
+}
+
+int row = ReadData("Введите кол-во строк: ");
+int column = ReadData("Введите кол-во столбцов: ");
+int[,] arr2D = Fill2DArray(row, column, 10, 99);
+Print2DArrayColor(arr2D);
