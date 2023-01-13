d5608cd319264b5d5652a75d3119ad9ac33916af
oppressor117 <oppressor117@gmail.com>
2023-01-13 16:25:45 +0300

    Solution for Homework_Task56 added

---
+++ Homework_Task56/Program.cs
@@ -0,0 +1,80 @@
+﻿//Задача 56: Задайте прямоугольный двумерный массив. 
+//Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+
+
+// Чтение данных из консоли
+int ReadData(string line)
+{
+    Console.Write(line);
+    int number = int.Parse(Console.ReadLine() ?? "0");
+    return number;
+}
+
+//Вывод массива на экран
+void Print2DArrayColor(int[,] matrix)
+{
+    ConsoleColor[] col = new ConsoleColor[]{ConsoleColor.Black,ConsoleColor.Blue,ConsoleColor.Cyan,
+                                        ConsoleColor.DarkBlue,ConsoleColor.DarkCyan,ConsoleColor.DarkGray,
+                                        ConsoleColor.DarkGreen,ConsoleColor.DarkMagenta,ConsoleColor.DarkRed,
+                                        ConsoleColor.DarkYellow,ConsoleColor.Gray,ConsoleColor.Green,
+                                        ConsoleColor.Magenta,ConsoleColor.Red,ConsoleColor.White,
+                                        ConsoleColor.Yellow};
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.ForegroundColor = col[matrix[i, j] % 15];
+            Console.Write(matrix[i, j] + "     ".Substring(matrix[i, j].ToString().Length));
+            Console.ResetColor();
+        }
+        Console.WriteLine();
+    }
+}
+
+// Генерируем двумерный массив, заполненный случайными числами
+int[,] Fill2DArray(int countRow, int countColumn, int topBorder, int downBorder)
+{
+    System.Random rand = new System.Random();
+    int[,] array2D = new int[countRow, countColumn];
+    for (int i = 0; i < countRow; i++)
+    {
+        for (int j = 0; j < countColumn; j++)
+        {
+            array2D[i, j] = rand.Next(topBorder, downBorder + 1);
+        }
+    }
+    return array2D;
+}
+
+//Выполнение поиска строки с наименьшей суммой 
+void MinRow(int[,] matrix)
+{
+    int index = 0;
+    int minSumm = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        int summ =0;
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            summ+= matrix[i,j];
+        }
+        if (i==0)
+        {
+            minSumm = summ;
+        }
+        else if (summ<minSumm)
+        {
+            minSumm = summ;
+            index = i;
+        }
+    }
+    Console.WriteLine($"Строка с наименьшей суммой элементов -> {index+1} ");  
+}        
+
+int row = ReadData("Введите количество строк ");                                 // Пользователь вводит количество строк
+int column = ReadData("Введите количество столбцов ");                           // Пользователь вводит количество столбцов
+int[,] arr2D = Fill2DArray(row, column, 0, 99);
+Print2DArrayColor(arr2D);
+Console.WriteLine();
+MinRow(arr2D);
+
