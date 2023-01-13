64951abf4da456a00d08b2b5e58604493733771b
oppressor117 <oppressor117@gmail.com>
2023-01-13 17:18:47 +0300

    Solution for Homework_Task58 added

---
+++ Homework_Task58/Program.cs
@@ -0,0 +1,79 @@
+﻿//Задача 58: Задайте две матрицы. 
+//Напишите программу, которая будет находить произведение двух матриц.
+
+// Чтение данных из консоли
+int ReadData(string line)
+{
+    Console.Write(line);
+    int number = int.Parse(Console.ReadLine() ?? "0");
+    return number;
+}
+
+//Вывод массива на экран
+void PrintMatrixColor(int[,] matrix)
+{
+    ConsoleColor[] col = new ConsoleColor[]{ConsoleColor.Black,ConsoleColor.Blue,ConsoleColor.Cyan,
+                                        ConsoleColor.DarkBlue,ConsoleColor.DarkCyan,ConsoleColor.DarkGray,
+                                        ConsoleColor.DarkGreen,ConsoleColor.DarkMagenta,ConsoleColor.DarkRed,
+                                        ConsoleColor.DarkYellow,ConsoleColor.Gray,ConsoleColor.Green,
+                                        ConsoleColor.Magenta,ConsoleColor.Red,ConsoleColor.White,
+                                        ConsoleColor.Yellow};
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.ForegroundColor = col[matrix[i, j] % 15];
+            Console.Write(matrix[i, j] + "     ".Substring(matrix[i, j].ToString().Length));
+            Console.ResetColor();
+        }
+        Console.WriteLine();
+    }
+}
+
+// Генерируем двумерный массив, заполненный случайными числами
+int[,] FillMatrix(int countRow, int countColumn, int topBorder, int downBorder)
+{
+    System.Random rand = new System.Random();
+    int[,] matrix = new int[countRow, countColumn];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rand.Next(topBorder, downBorder + 1);
+        }
+    }
+    return matrix;
+}
+
+int row = ReadData("Введите количество строк ");                                 // Пользователь вводит количество строк
+int column = ReadData("Введите количество столбцов ");  
+int[,] matrix = FillMatrix(row, column, 1, 10);
+PrintMatrixColor(matrix);
+Console.WriteLine();
+
+int[,] secondMatrix = FillMatrix(row, column, 1, 10);
+PrintMatrixColor(secondMatrix);
+Console.WriteLine();
+
+int[,] resultMatrix = FillMatrix(row, column, 1, 10);
+
+
+
+if (matrix.GetLength(0) != secondMatrix.GetLength(1))
+{
+    Console.WriteLine("Нельзя перемножить");
+    return;
+}
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < secondMatrix.GetLength(1); j++)
+    {
+        resultMatrix[i, j] = 0;
+        for (int k = 0; k < matrix.GetLength(1); k++)
+        {
+            resultMatrix[i, j] += matrix[i, k] * secondMatrix[k, j];
+        }
+    }
+}
+
+PrintMatrixColor(resultMatrix);
