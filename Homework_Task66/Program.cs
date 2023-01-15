b30b1c3b6ae5603adce757b75d27acb55fa71b62
oppressor117 <oppressor117@gmail.com>
2023-01-15 18:18:28 +0300

    Solution for Homework_Task66 added

---
+++ Homework_Task66/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 66
+// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// Чтение данных из консоли
+int ReadData(string line)
+{
+    Console.Write(line);
+    int number = int.Parse(Console.ReadLine() ?? "0");
+    return number;
+}
+
+// Печать результата
+void PrintResult(int result)
+{
+    Console.WriteLine(result);
+}
+
+//Нахождение суммы через рекурсию
+int SumRecMN(int M, int N)
+{
+    if (M >= N) return N;
+    return M + SumRecMN(M + 1, N);
+}
+
+int number1 = ReadData("Введите число M: ");
+int number2 = ReadData("Введите число N: ");
+//int result = SumRecMN(number1, number2);
+int result = number1<number2? SumRecMN(number1, number2):SumRecMN(number2, number1);
+PrintResult(result);
