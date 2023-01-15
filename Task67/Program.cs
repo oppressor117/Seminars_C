7c1ddb64f7043e484104d085140951a95e8bdb0d
oppressor117 <oppressor117@gmail.com>
2023-01-15 17:50:39 +0300

    Solution for Task67 added

---
+++ Task67/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 67
+// Принимает на вход число и возвращает сумму цифр в числе
+
+int numberN = ReadData("Введите число N: ");
+int sum = RecSumDig(numberN);
+PrintResult(sum);
+
+int ReadData(string line)
+{
+    Console.Write(line);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+
+void PrintResult(int prefix)
+{
+    Console.Write(prefix);
+}
+
+int RecSumDig(int n)
+{
+    if (n<= 10) return n;
+    int result = n % 10 + RecSumDig(n/10);
+    return result;
+}
+
